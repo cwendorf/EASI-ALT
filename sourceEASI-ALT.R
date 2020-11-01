4b4bfe4a173613ccfcd8f0765d30802c49f0bb49
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-11-01 02:46:42 -0600

    Updated for EASI 2.200512

---
+++ sourceEASI-ALT.R
@@ -0,0 +1,13 @@
+# Estimation Approach to Statistical Inference
+## Source All Available EASI-ALT Functions
+
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Basic.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Data.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Summary.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/ANOVA.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Contrasts.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Difference.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Means.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Pairwise.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Regression.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Tukey.R")
