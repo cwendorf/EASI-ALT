fe56d36d5bcf76bbc24df3206948e4e79464c0ff
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-12-22 05:03:14 -0600

    Update sourceEASIalt.R

--- sourceEASIalt.R
+++ sourceEASIalt.R
@@ -1,13 +1,13 @@
 # Estimation Approach to Statistical Inference
-## Source All Available EASI-ALT Functions
+## Source All Available EASIalt Functions
 
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Basic.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Data.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Summary.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/ANOVA.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Contrasts.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Difference.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Means.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Pairwise.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Regression.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI-ALT/master/R/Tukey.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Basic.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Data.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Summary.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/ANOVA.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Contrasts.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Difference.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Means.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Pairwise.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Regression.R")
+source("http://raw.githubusercontent.com/cwendorf/EASIalt/master/R/Tukey.R")
