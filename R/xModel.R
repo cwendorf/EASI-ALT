4f44ffcfce291dd7e92e01b25cc6412da7384e13
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-01-19 05:16:10 -0600

    Update xModel.R

--- R/xModel.R
+++ R/xModel.R
@@ -6,6 +6,23 @@
 .pvaModel <- function(x,...) 
   UseMethod(".pvaModel")
 
+.pvaModel.default <- function(...){
+  data <- data.frame(...)
+  columns <- dim(data)[2]
+  dataLong <- reshape(data,varying=1:columns,v.names="Outcome",timevar="Variables",idvar="Subjects",direction="long")
+  dataLong$Subjects <- as.factor(dataLong$Subjects)
+  dataLong$Variables <- as.factor(dataLong$Variables)
+  model <- lm(Outcome~Variables+Subjects,data=dataLong)
+  summ <- summary(model)
+  adjr2 <- summ$adj.r.squared
+  r2 <- summ$r.squared
+  r <- sqrt(r2)
+  results <- cbind(r,r2,adjr2)
+  colnames(results) <- c("R","R2","AdjR2")
+  rownames(results)="Model"
+  round(results,3)
+}
+
 .pvaModel.formula <- function(formula,...) {
   model <- lm(formula)
   summ <- summary(model)
