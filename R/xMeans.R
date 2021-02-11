3d9806d8bd493bea5c7720356cf09a0b766525b4
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-02-11 12:30:01 -0600

    Updated mean functions

--- R/xMeans.R
+++ R/xMeans.R
@@ -8,50 +8,59 @@
 
 .dsMeans.default <- function(...){
   data <- data.frame(...)
-  results <- data.frame(matrix(ncol=3,nrow=0))
-  for (i in 1:ncol(data)) results[i,] <- .ds(data[,i])
-  colnames(results) <- c("N","M","SD")
-  rownames(results) <- colnames(data)
-  results
+  rn <- names(data)
+  M <- sapply(data,mean,na.rm=TRUE)
+  SD <- sapply(data,sd,na.rm=TRUE) 
+  results <- cbind(N=N,M=M,SD=SD)
+  rownames(results) <- rn
+  round(results,3)
 }
 
 .dsMeans.formula <- function(formula,...) {
-  results <- aggregate(formula,FUN=.ds,...)
-  rn <- results[,1]
-  results <- results[[2]]
+  rn <- aggregate(formula,FUN=length)[[1]]
+  N <- aggregate(formula,FUN=length)[[2]]
+  M <- aggregate(formula,FUN=mean,na.rm=TRUE)[[2]]
+  SD <- aggregate(formula,FUN=sd,na.rm=TRUE)[[2]]
+  results <- cbind(N=N,M=M,SD=SD)
   rownames(results) <- rn
-  results
+  round(results,3)
 }
 
 describeMeans <- function(...){
   cat("\nDESCRIPTIVE STATISTICS FOR THE DATA\n\n")
   print(.dsMeans(...))
   cat("\n")
-}
+}  
 
 ### Confidence Interval Functions 
 
 .ciMeans <- function(x,...) 
   UseMethod(".ciMeans")
 
-.ciMeans.default <- function(...,conf.level=.95){
+.ciMeans.default <- function(...,conf.level=.95) {
   data <- data.frame(...)
-  results <- data.frame(matrix(ncol=5,nrow=0))
-  for (i in 1:ncol(data)) results[i,] <- .ci(data[,i],conf.level=conf.level)
-  colnames(results) <- c("M","SE","df","LL","UL")
-  rownames(results) <- colnames(data)
-  results
+  columns <- dim(data)[2]
+  dataLong <- reshape(data,varying=1:columns,v.names="Outcome",timevar="Variables",idvar="Subjects",direction="long")
+  dataLong$Subjects <- as.factor(dataLong$Subjects)
+  dataLong$Variables <- as.factor(dataLong$Variables)
+  model <- lm(Outcome~Variables-1,data=dataLong)
+  results <- cbind(summary(model)$coeff[,1:2],confint(model,level=conf.level))
+  colnames(results) <- c("M","SE","LL","UL")
+  rownames(results) <- names(data)
+  round(results,3)
 }
 
-.ciMeans.formula <- function(formula,...) {
-  results <- aggregate(formula,FUN=.ci,...)
-  rn <- results[,1]
-  results <- results[[2]]
-  rownames(results) <- rn
-  results
+.ciMeans.formula <- function(formula,conf.level=.95,...) {
+  x <- eval(formula[[3]])
+  formula <- update(formula, ~ . + -1)
+  model <- lm(formula)
+  results <- cbind(summary(model)$coeff[,1:2],confint(model,level=conf.level))
+  colnames(results) <- c("M","SE","LL","UL")
+  rownames(results) <- levels(x)
+  round(results,3)
 }
 
-estimateMeans <- function(...){
+estimateMeans <- function(...) {
   cat("\nCONFIDENCE INTERVALS FOR THE MEANS\n\n")
   print(.ciMeans(...))
   cat("\n")
@@ -62,30 +71,71 @@ estimateMeans <- function(...){
 .nhstMeans <- function(x,...) 
   UseMethod(".nhstMeans")
 
-.nhstMeans.default <- function(...,mu=0){
+.nhstMeans.default <- function(...,mu=0) {
   data <- data.frame(...)
-  results <- data.frame(matrix(ncol=5,nrow=0))
-  for (i in 1:ncol(data)) results[i,] <- .nhst(data[,i],mu=mu)
-  colnames(results) <- c("Diff","SE","t","df","p")
-  rownames(results) <- colnames(data)
-  results
+  columns <- dim(data)[2]
+  dataLong <- reshape(data,varying=1:columns,v.names="Outcome",timevar="Variables",idvar="Subjects",direction="long")
+  dataLong$Subjects <- as.factor(dataLong$Subjects)
+  dataLong$Variables <- as.factor(dataLong$Variables)
+  model <- lm(Outcome~Variables-1,data=dataLong)
+  results <- summary(model)$coeff
+  results[,1] <- results[,1]-mu
+  results[,3] <- results[,1]/results[,2]
+  results[,4] <- 2*pt(-abs(results[,3]),df=model$df.residual)
+  colnames(results) <- c("Diff","SE","t","p")
+  rownames(results) <- names(data)
+  round(results,3)  
 }
 
-.nhstMeans.formula <- function(formula,...) {
-  results <- aggregate(formula,FUN=.nhst,...)
-  rn <- results[,1]
-  results <- results[[2]]
-  rownames(results) <- rn
-  results
+.nhstMeans.formula <- function(formula,mu=0) {
+  x <- eval(formula[[3]])
+  formula <- update(formula, ~ . + -1)
+  model <- lm(formula)
+  results <- summary(model)$coeff
+  results[,1] <- results[,1]-mu
+  results[,3] <- results[,1]/results[,2]
+  results[,4] <- 2*pt(-abs(results[,3]),df=model$df.residual)
+  colnames(results) <- c("Diff","SE","t","p")
+  rownames(results) <- levels(x)
+  round(results,3)
 }
 
-testMeans <- function(...){
+testMeans <- function(...) {
   cat("\nHYPOTHESIS TESTS FOR THE MEANS\n\n")
   print(.nhstMeans(...))
   cat("\n")
 }
 
 ### Confidence Interval Plot Functions
+
+.ciPlot <- function(results,main,ylab,xlab,mu){
+  plot(results[,1],xaxt='n',xlim=c(.5,nrow(results)+.5),ylim=c(floor(min(results[,2])/2)*2,ceiling(max(results[,3])/2)*2),xlab=xlab,cex.lab=1.15,ylab=ylab,main=main,las=1,cex=1.5,pch=16,bty="l")
+  axis(1, 1:nrow(results), row.names(results))
+  for (i in 1:nrow(results)) lines(x=c(i,i),y=c(results[,2][i],results[,3][i]),lwd=2)
+  for (i in 1:nrow(results)) text(i,results[,1][i],results[,1][i],cex=.8,pos=2,offset=.5,font=2)
+  for (i in 1:nrow(results)) text(i,results[,2][i],results[,2][i],cex=.8,pos=2,offset=.5)  
+  for (i in 1:nrow(results)) text(i,results[,3][i],results[,3][i],cex=.8,pos=2,offset=.5)
+  if (!is.null(mu)) {abline(h=mu,lty=2)}
+}
+
+.diffPlot <- function(results,graph,main,ylab,xlab){
+  plot(c(1,2,3),graph[,1],xaxt="n",xlim=c(.4,3.6),ylim=c(floor(min(graph[,2])/2)*2,ceiling(max(graph[,3])/2)*2),pch=c(16,16,17),cex=1.5,xlab=xlab,ylab=ylab,main=main,las=1,cex.lab=1.15,bty="l")
+  axis(1,at=c(1,2,3),labels=rownames(graph))
+  for (i in 1:3) lines(x=c(i,i), y=c(graph[,2][i],graph[,3][i]),lwd=2)
+  for (i in 1:2) text(i,graph[,1][i],graph[,1][i],cex=.8,pos=2,offset=.5,font=2)
+  for (i in 1:2) text(i,graph[,2][i],graph[,2][i],cex=.8,pos=2,offset=.5)  
+  for (i in 1:2) text(i,graph[,3][i],graph[,3][i],cex=.8,pos=2,offset=.5)
+  text(3,graph[,1][3],results[,1][3],cex=.8,pos=4,offset=.5,font=2)
+  text(3,graph[,2][3],results[,2][3],cex=.8,pos=4,offset=.5)  
+  text(3,graph[,3][3],results[,3][3],cex=.8,pos=4,offset=.5)
+  arrows(1,graph[1,1],4.5,graph[1,1],code=3,length=0,lty=2)  
+  arrows(2,graph[2,1],4.5,graph[2,1],code=3,length=0,lty=2)
+  if(results[1,1]<results[2,1]) {td <- graph[1,1]-axTicks(4)[max(which(axTicks(4)<graph[1,1]))]}
+  if(results[1,1]>=results[2,1]) {td <- graph[1,1]-axTicks(4)[min(which(axTicks(4)>graph[1,1]))]}  
+  val <- axTicks(4)-graph[1,1]+td
+  loc <- axTicks(4)+td  
+  axis(4,at=loc,labels=val,las=1)
+}
 
 plotMeans <- function(...) 
   UseMethod("plotMeans")
@@ -108,3 +158,4 @@ plotMeans.formula <- function(formula,mu=NULL,...){
   row.names(results) <- levels(x)
   .ciPlot(results,main,ylab,xlab,mu)
 }
+
