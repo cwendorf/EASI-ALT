5f711df7d91e20290b126462644040f033660318
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-02-19 14:31:29 -0600

    Update xContrasts.R

--- R/xContrasts.R
+++ R/xContrasts.R
@@ -18,9 +18,10 @@
   model <- aov(Outcome~Variable+Error(Subjects),data=dataLong)
   first <- summary(lm(model))[[4]][1:vlevels,1:2]
   second <- confint(lm(model),level=conf.level)[1:vlevels,1:2]
-  results <- round(cbind(first,second),3)
-  colnames(results) <- c("Est","SE","LL","UL")
-  results
+  df <- lm(model)$df.residual
+  results <- cbind(first,df,second)
+  colnames(results) <- c("Est","SE","df","LL","UL")
+  round(results,3)
 }
 
 .ciContrasts.formula <- function(formula,contrasts=contr.sum,conf.level=.95,...){
@@ -28,9 +29,9 @@
   y <- eval(formula[[2]])
   contrasts(x) <- contrasts
   model <- lm(y~x,...)
-  results <- round(cbind(summary(model)[[4]][,1:2],confint(model,level=conf.level)),3)
-  colnames(results) <- c("Est","SE","LL","UL")
-  results
+  results <- cbind(summary(model)[[4]][,1:2],model$df.residual,confint(model,level=conf.level))
+  colnames(results) <- c("Est","SE","df","LL","UL")
+  round(results,3)
 }
 
 estimateContrasts <- function(...){
@@ -54,10 +55,10 @@ estimateContrasts <- function(...){
   contrasts(dataLong$Variable) <- contrasts
   contrasts(dataLong$Subjects) <- contr.sum
   model <- aov(Outcome~Variable+Error(Subjects),data=dataLong)
-  first <- summary(lm(model))[[4]][1:vlevels,1:4]
-  results <- round(first,3)
-  colnames(results) <- c("Diff","SE","t","p")
-  results
+  df <- lm(model)$df.residual
+  results <- cbind(summary(lm(model))[[4]][1:vlevels,1:3],df,summary(lm(model))[[4]][1:vlevels,4])
+  colnames(results) <- c("Diff","SE","t","df","p")
+  round(results,3)
 }
 
 .nhstContrasts.formula <- function(formula,contrasts=contr.sum,...){
@@ -65,9 +66,10 @@ estimateContrasts <- function(...){
   y <- eval(formula[[2]])
   contrasts(x) <- contrasts
   model <- lm(y~x,...)
-  results <- round(summary(model)[[4]][,],3)
-  colnames(results) <- c("Diff","SE","t","p")
-  results
+  df <- lm(model)$df.residual  
+  results <- cbind(summary(lm(model))[[4]][,1:3],df,summary(lm(model))[[4]][,4])
+  colnames(results) <- c("Diff","SE","t","df","p")
+  round(results,3)
 }
 
 testContrasts <- function(...){
@@ -86,7 +88,7 @@ plotContrasts.default <- function(...,mu=NULL) {
   main <- "Confidence Intervals for the Contrasts"
   ylab <- "Mean Difference"
   xlab <- ""
-  results <- .ciContrasts(...)[,c(1,3,4)]
+  results <- .ciContrasts(...)[,c(1,4,5)]
   .ciPlot(results,main,ylab,xlab,mu)
 }  
 
@@ -94,6 +96,6 @@ plotContrasts.formula <- function(formula,mu=NULL,...) {
   main <- "Confidence Intervals for the Contrasts"
   ylab <- "Mean Difference"
   xlab <- ""
-  results <- .ciContrasts(formula,...)[,c(1,3,4)]
+  results <- .ciContrasts(formula,...)[,c(1,4,5)]
   .ciPlot(results,main,ylab,xlab,mu)
 }
